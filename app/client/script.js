f66d86c392feb27b345584c5d2712833bd41d44d
Kirill <smirnowku@gmail.com>
2017-12-11 01:05:55 +0300

    implement show list, select item, display markdown

--- app/client/script.js
+++ app/client/script.js
@@ -1,4 +1,4 @@
-new Vue({
+const addMd = new Vue({
     el: '#add',
     data: {
         mdName: ''
@@ -6,10 +6,52 @@ new Vue({
     methods: {
         createMd: function () {
             $.post('/api/md', {
-                    "name": this.mdName
-            }, function (response, status) {
-                console.log(status + ": " + response);
+                "name": this.mdName
+            }, function () {
+                refresh();
+            }).fail(function (error) {
+                alert(error.responseText);
             });
         }
     }
 });
+
+const mdList = new Vue({
+    el: "#mdList",
+    data: {
+        mds: []
+    },
+    methods: {
+        mdSelect: function (md) {
+            editor.$data.content = md.content;
+            viewer.update();
+        }
+    }
+});
+
+const editor = new Vue({
+    el: "#editor",
+    data: {
+        content: ''
+    }
+});
+
+const viewer = new Vue({
+    el: "#viewer",
+    data: {
+        content: ''
+    },
+    methods: {
+        update: function () {
+            this.content = marked(editor.$data.content);
+        }
+    }
+});
+
+function refresh() {
+    $.get('/api/md', function (response, status) {
+        mdList.$data.mds = response;
+    });
+}
+
+refresh();
